cbbf5ffd59cc1db424023b60a76f21817c345c89
trishaad <37005597+trishaad@users.noreply.github.com>
2018-03-28 19:54:20 +0300

    Update Project.java

--- Split-it/Project.java
+++ Split-it/Project.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package MainMenu;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,108 +7,215 @@ import java.util.Set;
  *
  * @author trishaadbsurty
  */
-public class Project {
-    
-private String theName;
-private int teamSize;
-private Set<String> teamList;
-private HashMap<String, HashMap<String, Integer>> votes;
-
-
-Project (String teamName, int memberSize, Set<String> nameList)
+public class Project 
 {
-    theName = teamName;
-    teamSize = memberSize;
-    teamList = nameList;
-    
-    votes = new HashMap<String, HashMap<String, Integer>>();
-    
-    for (String memberName: nameList) {
-        HashMap<String, Integer> otherMembers = new HashMap<String, Integer>();
-        
-        for (String otherMemberName: nameList) {
-            if (!otherMemberName.equals(memberName)) {
-                otherMembers.put(otherMemberName, 0);
+    private String theName;
+    private int teamSize;
+    private Set<String> teamList;
+    private LinkedHashMap<String, LinkedHashMap<String, Integer>> votes;
+
+    /**
+     *
+     * @param teamName
+     * @param memberSize
+     * @param nameList
+     */
+    Project (String teamName, int memberSize, Set<String> nameList)
+    {
+        theName = teamName;
+        teamSize = memberSize;
+        teamList = nameList;
+        votes = new LinkedHashMap<String, LinkedHashMap<String, Integer>>();
+        for (String memberName: nameList) 
+        {
+            LinkedHashMap<String, Integer> otherMembers = new LinkedHashMap<String, Integer>();
+            for (String otherMemberName: nameList) 
+            {
+                if (!otherMemberName.equals(memberName)) 
+                {
+                    otherMembers.put(otherMemberName, 0);
+                }
             }
+        
+            votes.put(memberName, otherMembers);
         }
-        
-        votes.put(memberName, otherMembers);
     }
-}
-
- //Creates a String which is a textual representation of the Project object.
     
-@Override
-public String toString()
-{
-    
-     return (theName + ","
-             + teamSize + ","
-             + printTeamList() + ","
-             + printVotes() + "\n");
-}
+    //Creates a String which is a textual representation of the Project object.    
 
-public String getTheName()
-{
-    return theName;
-}
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString()
+    {
+        return (theName + "," + teamSize + "," + printTeamList() + "," + printVotesFile() + "\n");
+    }
 
-public Integer getTheTeamSize()
-{
-    return teamSize;
-}
+    /**
+     *
+     * @return
+     */
+    public String getTheName()
+    {
+        return theName;
+    }
 
-public Set<String> getTeamList()
-{
-    return teamList;
-}
+    /**
+     *
+     * @return
+     */
+    public Integer getTheTeamSize()
+    {
+        return teamSize;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Set<String> getTeamList()
+    {
+        return teamList;
+    }
   
-public String printTeamList()
-{
-            String theList = "";
-            String comma = ",";
-        
+    //Prints the team names in the required format to be stored in the file
+    
+    /**
+     *
+     * @return
+     */
+    public String printTeamList()
+    {
+        String theList = "";
+        String comma = ",";
         for (String memberName : teamList)
         {
             theList += (memberName + comma);
         }
-    //removes comma at the end of the team list    
-    return theList.substring(0, theList.length() -1);    
-}
+    
+        //Removes comma at the end of the team list    
+        return theList.substring(0, theList.length() -1);    
+    }
 
-public HashMap<String, HashMap<String, Integer>> getVotes()
-{
-    return votes;
-}
-
-public Set<String> otherMembers(String memberName){
-    return votes.get(memberName).keySet();
-}
-
-public void recordVotes(String teamMember, HashMap<String, Integer> teamMemberVotes)
-{
-    votes.put(teamMember, teamMemberVotes);
-}
-
-public String printVotes()
-{ 
-    String voteString = "";
-    String comma = ",";
-    for (Map.Entry<String,HashMap<String, Integer>> pair : votes.entrySet()) 
+    /**
+     *
+     * @return
+     */
+    public LinkedHashMap<String, LinkedHashMap<String, Integer>> getVotes()
     {
-        String voterName = pair.getKey();
-        voteString += (voterName + comma);        
-        HashMap<String, Integer> memberVotes = pair.getValue();
-        for (Map.Entry<String, Integer> memberVote : memberVotes.entrySet()) 
+        return votes;
+    }
+
+    //Returns all other members for a given member name
+
+    /**
+     *
+     * @param memberName
+     * @return
+     */
+    public Set<String> otherMembers(String memberName)
+    {
+        return votes.get(memberName).keySet();
+    }
+
+    //Sets each members votes into the hashmap
+    
+    /**
+     *
+     * @param teamMember
+     * @param teamMemberVotes
+     */
+    public void recordVotes(String teamMember, LinkedHashMap<String, Integer> teamMemberVotes)
+    {
+        votes.put(teamMember, teamMemberVotes);
+    }
+
+    //Prints the votes out in the required format to be stored in the file
+    
+    /**
+     *
+     * @return
+     */
+    public String printVotesFile()
+    { 
+        String voteString = "";
+        String comma = ",";
+        for (Map.Entry<String,LinkedHashMap<String, Integer>> pair : votes.entrySet()) 
         {
-            String votedMemberName = memberVote.getKey();
-            String votePoint = memberVote.getValue().toString();
-            voteString += (votedMemberName + comma + votePoint + comma);            
+            String voterName = pair.getKey();
+            voteString += (voterName + comma);        
+            LinkedHashMap<String, Integer> memberVotes = pair.getValue();
+            for (Map.Entry<String, Integer> memberVote : memberVotes.entrySet()) 
+            {
+                String votedMemberName = memberVote.getKey();
+                String votePoint = memberVote.getValue().toString();
+                voteString += (votedMemberName + comma + votePoint + comma);            
+            }
         }
+    
+        //Removes comma at the end of the vote string  
+        return voteString.substring(0, voteString.length() -1);   
+    }   
+    
+    //Prints the votes out for each member so user can view them 
+    public void printVotesDisplay()
+    {
+        {
+            System.out.println("\nVotes for the project: " + theName);
+            for (Map.Entry<String,LinkedHashMap<String, Integer>> pair : getVotes().entrySet()) 
+            {
+                String voterName = pair.getKey();
+                System.out.println("\nThese are " + voterName + "'s votes:\n");       
+                LinkedHashMap<String, Integer> memberVotes = pair.getValue();
+                for (Map.Entry<String, Integer> memberVote : memberVotes.entrySet()) 
+                {
+                    String votedMemberName = memberVote.getKey();
+                    String votePoint = memberVote.getValue().toString();
+                    System.out.println("\t" + votedMemberName + ": " + votePoint + " points");            
+                }
+            }
+        }        
     }
     
-   //removes comma at the end of the vote string  
-   return voteString.substring(0, voteString.length() -1);   
-}
-        
+    //Rebuilds entire project from line of data
+    
+    /**
+     *
+     * @param projectName
+     * @param projectLine
+     * @param projectFields
+     * @return
+     */
+    public static Project buildFromLine(String projectName, String projectLine, String[] projectFields) 
+    {  
+        projectFields = projectLine.split(",");
+        int teamSize = Integer.parseInt(projectFields[1]);
+        LinkedHashSet<String> teamList = new LinkedHashSet<>();
+        for (int i = 2; i < 2 + teamSize; i++)
+        {
+            teamList.add(projectFields[i]);
+        }
+
+        Project existingProject = new Project(projectName, teamSize, teamList);
+        int currentIndex = 1 + teamSize;
+        for (int i = 0; i < teamSize; i++)
+        {
+            currentIndex += 1;
+            String voterName = projectFields[currentIndex];
+            LinkedHashMap<String,Integer> votes = new LinkedHashMap<>();
+            for (int j = 0; j < teamSize - 1; j++) 
+            {
+                String votedFor = projectFields[currentIndex + 1];
+                Integer votePoint = Integer.parseInt(projectFields[currentIndex + 2]);
+                votes.put(votedFor, votePoint);
+                currentIndex += 2;
+            }
+
+            existingProject.recordVotes(voterName, votes);
+        }
+
+        return existingProject;
+    }    
 }
